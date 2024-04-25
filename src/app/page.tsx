bc0d059df18b193824f056cf836addb0125b360a
rugo-cafe <rugyasha@gmail.com>
2024-04-25 00:58:22 -0300

    teste

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,92 @@
-import Image from "next/image";
+  import Image from "next/image";
+  "use client";
+  import { useState } from "react";
+  import Link from 'next/link';
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+  export default function Home() {
+
+    const [darkMode, setDarkMode] = useState(false);
+
+    const toggleDarkMode = () => {
+      setDarkMode (!darkMode);
+    }
+
+    return ( <>
+    <html className={`${darkMode && "dark"}`}>
+      <head>
+      <link rel="icon" href="/logo-dark.svg" />
+      <link href="https://fonts.cdnfonts.com/css/general-sans" rel="stylesheet" />
+      <link href="/fonts/Gambetta-Regular.woff2" rel="stylesheet" />
+      <title>vitor hugo cunha</title>
+      <style>
+              {`
+                .btn-drop-shadow {
+                  transition: transform 1s ease-in-out;
+                }
+                .btn-drop-shadow:hover {
+                  transform: translateY(-5px);
+                }
+              `}
+            </style>
+      </head>
+
+
+      <body className="bg-main-bg dark:bg-purp subpixel-antialiased">
+      
+      
+        <div className="flex flex-row justify-center items-center">
+          <Link href="/">
+          <img className="mt-10 lg:w-18 sm:w-14 w-8 block dark:hidden" src="/logo.svg" alt="logo" />
+          </Link>
+          <Link href="/">
+          <img className="mt-10 lg:w-18 sm:w-14 w-8 hidden dark:block" src="/logo-dark.svg" alt="logo dark mode" />
+          </Link>
+          <Link href="/">
+          <h1 className="mt-10 ml-3 lg:text-7xl sm:text-6xl text-4xl font-bold text-purp dark:text-main-bg">vitor hugo cunha</h1>
+          </Link>  
         </div>
+
+        <div>
+          <button className="btn-drop-shadow hover:drop-shadow-bt delay-500 flex items-center justify-center absolute sm:w-16 sm:h-16 w-10 h-10 bottom-16 right-16 bg-purp dark:bg-main-bg rounded-full text-main-bg dark:text-purp" onClick={toggleDarkMode}>
+        
+          <svg className="sm:stroke-1.5 sm:w-8 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M12 18v-5.25m0 0a6.01 6.01 0 0 0 1.5-.189m-1.5.189a6.01 6.01 0 0 1-1.5-.189m3.75 7.478a12.06 12.06 0 0 1-4.5 0m3.75 2.383a14.406 14.406 0 0 1-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 1 0-7.517 0c.85.493 1.509 1.333 1.509 2.316V18" />
+          </svg>
+
+          </button>
+    
+        </div>
+          <h1 className="sm:mt-20 mt-16  text-center lg:text-7xl sm:text-6xl text-4xl font-gambetta font-bold text-owange">designer, criativo.</h1>
+          <h1 className="sm:mt-20 mt-16 text-center lg:text-9xl sm:text-8xl text-5xl font-gambetta font-bold text-dpurp">site em construção.</h1>
+        <div>
+
+        </div>
+      </body>
+          
+      <div className="flex flex-col min-h-screen">
+        <main className="flex-1"></main>
+        <footer className=" bg-owange rounded-lg shadow m-4 dark:bg-dpurp dark:opacity-70 dark:drop-shadow-bt">
+            <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+              <span className="text-sm text-purp sm:text-center dark:text-main-bg hover:drop-shadow-dw">feito com ❤️ por vitor hugo cunha 
+            </span>
+            <ul className="flex flex-wrap items-center mt-3 text-sm font-semibold text-purp dark:text-main-bg hover:drop-shadow-xl sm:mt-0">
+                <li>
+                    <a href="#" className="hover:drop-shadow-bt me-4 md:me-6">sobre</a>
+                </li>
+
+                <li>
+                    <a href="#" className="hover:drop-shadow-bt me-4 md:me-6">projetos</a>
+                </li>
+                <li>
+                    <a href="#" className="hover:drop-shadow-dw">contato</a>
+                </li>
+            </ul>
+            
+            </div>
+        </footer>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
-}
+      
+      </html>
+      </>
+    );
+  }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,15 +6,29 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+
+  darkMode: "selector",
+  
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      dropShadow: { 
+        'bt': '-3px 6px 11px rgba(0, 0, 0, 0.25)',
+        'dw': '-3px 6px 11px rgba(245, 0, 0, 1'
+      },
+      fontFamily: {
+        general: ["General Sans"],
+        gambetta: ["Gambetta"]
+      },
+      colors: {
+        'main-bg': '#F5E9BF',
+        'greenish': '#788374',
+        'owange': '#AA644D',
+        'purp': '#372A39',
+        'dpurp': '#1F0F21'
       },
     },
   },
   plugins: [],
+  
 };
 export default config;
